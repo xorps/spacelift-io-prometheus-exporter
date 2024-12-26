af50ac68e9209b86641e8511a57fc83b725c9a3c
xorps <758027+xorps@users.noreply.github.com>
2024-12-26 16:06:07 -0700

    add filesystem support for api keys

--- client/session/api_key.go
+++ client/session/api_key.go
@@ -2,8 +2,10 @@ package session
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/hasura/go-graphql-client"
@@ -11,7 +13,11 @@ import (
 
 // FromAPIKey builds a Spacelift session from a combination of endpoint, API key
 // ID and API key secret.
-func FromAPIKey(ctx context.Context, client *http.Client, endpoint, keyID, keySecret string) (Session, error) {
+func FromAPIKey(ctx context.Context, client *http.Client, endpoint string, keyID string, keySecret Secret) (Session, error) {
+	if keySecret == nil {
+		return nil, errors.New("keySecret is nil")
+	}
+
 	out := &apiKey{
 		apiToken: apiToken{
 			client:   client,
@@ -31,7 +37,8 @@ func FromAPIKey(ctx context.Context, client *http.Client, endpoint, keyID, keySe
 
 type apiKey struct {
 	apiToken
-	keyID, keySecret string
+	keyID     string
+	keySecret Secret
 }
 
 func (g *apiKey) BearerToken(ctx context.Context) (string, error) {
@@ -53,9 +60,14 @@ func (g *apiKey) exchange(ctx context.Context) error {
 		APIKeyUser user `graphql:"apiKeyUser(id: $id, secret: $secret)"`
 	}
 
+	secret, err := g.keySecret.read(ctx)
+	if err != nil {
+		return fmt.Errorf("could not read secret: %w", err)
+	}
+
 	variables := map[string]interface{}{
 		"id":     graphql.ID(g.keyID),
-		"secret": graphql.String(g.keySecret),
+		"secret": graphql.String(secret),
 	}
 
 	if err := g.mutate(ctx, &mutation, variables); err != nil {
@@ -65,4 +77,42 @@ func (g *apiKey) exchange(ctx context.Context) error {
 	g.setJWT(&mutation.APIKeyUser)
 
 	return nil
+}
+
+type Secret interface {
+	read(ctx context.Context) (string, error)
+}
+
+type InlineSecret struct {
+	secret string
+}
+
+var _ Secret = (*InlineSecret)(nil)
+
+func NewInlineSecret(secret string) *InlineSecret {
+	return &InlineSecret{secret}
+}
+
+func (s *InlineSecret) read(_ context.Context) (string, error) {
+	return s.secret, nil
+}
+
+type FileSecret struct {
+	filepath string
+}
+
+var _ Secret = (*FileSecret)(nil)
+
+func NewFileSecret(filepath string) *FileSecret {
+	return &FileSecret{filepath}
+}
+
+func (f *FileSecret) read(_ context.Context) (string, error) {
+	// stdlib doesn't support context for File IO -- https://github.com/golang/go/issues/20280
+	bytes, err := os.ReadFile(f.filepath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
 }
